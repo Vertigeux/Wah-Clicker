2c7025ffdcfd892745eaa1711274cb95330dfd47
Vertigeux <jiffyeljefe@gmail.com>
2019-10-07 19:25:54 -0500

    Added audio concurrency

--- javascript/clicker.js
+++ javascript/clicker.js
@@ -1,13 +1,23 @@
 var wahCount = 0;
-var wahAudio = new Audio("res/wah.mp3")
+var currentWah = 0;
+var autoWahCount = 0;
 
 function wahEvent(){
+	var wahAudio = new Audio("res/wah.mp3");
 	wahCount++;
+	currentWah++;
 	document.getElementById("totalWah").innerHTML = wahCount;
-	document.getElementById('waluigiPicture').src='res/waluigiOpen.jpg'
+	document.getElementById('waluigiPicture').src='res/waluigiOpen.jpg';
 	wahAudio.play();
-	wahAudio.onended = function(){document.getElementById('waluigiPicture').src='res/waluigi.jpg'}
+	wahAudio.onended = wahEventEnd;
 	
+}
+
+function wahEventEnd(){
+	currentWah--;
+	if(currentWah == 0){
+		document.getElementById('waluigiPicture').src='res/waluigi.jpg'
+	}
 }
 
 window.onload = function() {
